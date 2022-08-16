78425577b258716f8848bb550cf799de7ca607d0
ivosam <ivgest@gmail.com>
2022-08-16 17:42:55 -0300

    primeiro commit

---
+++ src/main/java/com/ivosam/soap/webservice/customeradministration/CustomerAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.ivosam.soap.webservice.customeradministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAdministrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAdministrationApplication.class, args);
+    }
+
+}
