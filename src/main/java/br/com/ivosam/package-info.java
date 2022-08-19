4b6f6dba85309a3bd29aca03a638f40f8d69eb18
ivosam <ivgest@gmail.com>
2022-08-19 14:09:31 -0300

    finalizacao do servidor web

---
+++ src/main/java/br/com/ivosam/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
+// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
+// Gerado em: 2022.08.19 às 08:55:13 AM BRT 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ivosam.com.br", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package br.com.ivosam;
